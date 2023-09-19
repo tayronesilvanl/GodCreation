b895a039ab2549fa092a796910ee9e12eed1ecd2
Tayrone da Silva <121695470+tayronesilvanl@users.noreply.github.com>
2023-09-19 15:14:55 +0200

    Update app.py
    Nee functions to request the description of the image

--- app.py
+++ app.py
@@ -5,9 +5,24 @@ app = Flask(__name__)
 
 NASA_API_KEY = '5doj8kpbe88fbkRylonOYK1br6CqhYtVRbV92zEJ'  # Use a chave DEMO para testes iniciais. Recomendo obter sua própria chave mais tarde.
 
+def fraseDoDia():
+    category = 'knowledge'
+    api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
+    response2 = requests.get(api_url, headers={'X-Api-Key': '1bmLtfrb8+xKIqNsYJEgjQ==jdUm8a8JMT9c3Apq'})
+    if response2.status_code == requests.codes.ok:
+        quote = response2.json()
+        return quote
+    else:
+         return("Error: Frase do dia", response2.status_code, response2.text)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/fetch-quote', methods=['GET'])
+def fetchQuote():
+    return fraseDoDia()
 
 @app.route('/fetch-Image/<date>', methods=['GET'])
 def fetch_image(date):
@@ -15,6 +30,7 @@ def fetch_image(date):
     response = requests.get(APOD_URL)
     if response.status_code == 200:
         data = response.json()
+
         
         return jsonify(data)
     else:
@@ -23,4 +39,6 @@ def fetch_image(date):
     
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
